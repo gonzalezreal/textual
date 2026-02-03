1cd33140f4e0c471dbb76a2fc82075234a728e45
David van Dugteren <davidvandugteren@heidihealth.com>
2026-02-03 18:09:07 +1100

    Enable library evolution for framework compatibility

--- Package.swift
+++ Package.swift
@@ -32,6 +32,7 @@ let package = Package(
       swiftSettings: [
         .define("TEXTUAL_ENABLE_LINKS", .when(platforms: [.macOS, .iOS, .watchOS, .visionOS])),
         .define("TEXTUAL_ENABLE_TEXT_SELECTION", .when(platforms: [.macOS, .iOS, .visionOS])),
+        .unsafeFlags(["-enable-library-evolution"]),
       ]
     ),
     .testTarget(
